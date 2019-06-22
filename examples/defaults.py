2adbb19bf36ba8b88cf84e893984c1435ac552d6
Eliot Quon <eliot.quon@nrel.gov>
2019-06-22 17:50:48 -0600

    Make gaussian markers smaller in example

--- examples/defaults.py
+++ examples/defaults.py
@@ -8,24 +8,24 @@ styles = {
         #'label':r'1-D Gaussian ($\sigma=R$)',
         'color':'b',
         'linestyle':'--','linewidth':2,
-        'marker':'o','markersize':6
+        'marker':'o','markersize':4
     },
     '1-D Gaussian (Bastankhah)': {
         'color':'r',
         'linestyle':'--','linewidth':2,
-        'marker':'o','markersize':6
+        'marker':'o','markersize':4
     },
     '1-D Gaussian (ideal sigma)': {
         #'label':r'1-D Gaussian (x-mom deficit $\sigma$)',
         'color':'m',
         'linestyle':'--','linewidth':2,
-        'marker':'o','markersize':6
+        'marker':'o','markersize':4
     },
     '2-D Gaussian': {
         #'label':r'2-D Gaussian (opt $\sigma_x$,$\sigma_y$,$\theta$)',
         'color':'g',
         'linestyle':'--','linewidth':2,
-        'marker':'o','markersize':6
+        'marker':'o','markersize':4
     },
     
     'const area': {
